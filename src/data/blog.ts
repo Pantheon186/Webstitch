42352a2ec71a1375e4c292d7f08396d621eb123c
Pantheon186 <pantheonofworld186@gmail.com>
2025-08-07 23:53:44 +0530

    Fix blog.ts syntax error

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,1 +1,12 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+export interface BlogPost {
+  id: string;
+  title: string;
+  excerpt: string;
+  content: string;
+  author: string;
+  date: string;
+  tags: string[];
+  image?: string;
+}
+
+export const blogPosts: BlogPost[] = [];
